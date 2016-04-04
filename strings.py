3110eb7305d8e9581bb416688b12b859983380f7
felgari <felgari@gmail.com>
2016-04-04 16:44:58 +0200

    Added an algo to find the longest common substring in two strings.

--- strings.py
+++ strings.py
@@ -25,6 +25,7 @@ STR_1 = 'ABCDEFGFGFHIJK'
 SUBSTR_1 = 'FGFH'
 
 NUM_ARGS = 3
+ARGS_LCST = 4 
 
 # Looking for repeated substrings in a string.
 def Knuth_Morris_Pratt(s, subs):
@@ -44,10 +45,43 @@ def Knuth_Morris_Pratt(s, subs):
             s_idx += 1
     
     return -1
+
+def longest_common_substring(s1, s2):
+    """Reference:https://en.wikipedia.org/wiki/Longest_common_substring_problem
+    """
+    
+    len_s1 = len(s1)
+    len_s2 = len(s2)    
+    m = [ [0  for _ in range(len_s2) ] for _ in range(len_s1) ]
+    greatest_len = 0
+    common_str = ''
+    
+    for i in range(len_s1):
+        for j in range(len_s2):
+            if s1[i] == s2[j]:
+                
+                if i == 1 or j == 1:
+                    m[i][j] = 1
+                else:
+                    m[i][j] = m[i-1][j-1] + 1
+                    
+                if m[i][j] > greatest_len:
+                    greatest_len = m[i][j]
+                    common_str = s1[i-greatest_len+1:i] 
+                elif m[i][j] == greatest_len:
+                    common_str = common_str + s1[i-greatest_len+1:i]
+                    
+    return common_str
     
 if __name__ == "__main__":
     
-    if len(sys.argv) != NUM_ARGS:
-        print"ERROR: Falta argumento. Uso: %s numero" % sys.argv[0]
+    if len(sys.argv) < NUM_ARGS:
+        print"ERROR: Arguments needed. Use: %s num1 num2 ..." % sys.argv[0]
     else:
-        Knuth_Morris_Pratt(sys.argv[1], sys.argv[2])
+        print "A substring of %s in %s (with Knuth-Morris-Pratt) is at: %d" % \
+            (sys.argv[1], sys.argv[2], 
+             Knuth_Morris_Pratt(sys.argv[1], sys.argv[2]))
+        
+        print "Longest common substring of %s and %s is: %s" % \
+            (sys.argv[1], sys.argv[2], 
+             longest_common_substring(sys.argv[1], sys.argv[2]))
